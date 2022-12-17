c6e5c1a4d63a89e981078347bf56989a90213385
Gilad Robinzon <medicianhealthy@gmail.com>
2022-12-17 10:53:34 +0200

    Bugs and improvements

--- app/src/main/java/com/robinzon/medicationwizard/utils/DeviceConfig.java
+++
@@ -1,9 +0,0 @@
-package com.robinzon.medicationwizard.utils;
-
-import android.os.Build;
-
-public class DeviceConfig {
-    public static int getApiLevel(){
-        return Build.VERSION.SDK_INT;
-    }
-}
