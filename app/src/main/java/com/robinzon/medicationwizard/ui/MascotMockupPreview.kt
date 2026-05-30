883cab50293b55a8b68530af618d6323f0d51bfe
Gilad Robinzon <gili1012@gmail.com>
2026-05-30 14:52:17 +0300

    New icon design

---
+++ app/src/main/java/com/robinzon/medicationwizard/ui/MascotMockupPreview.kt
@@ -0,0 +1,35 @@
+package com.robinzon.medicationwizard.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.robinzon.medicationwizard.R
+
+/**
+ * A temporary preview tool to visualize the re-created Owl Doctor mascot.
+ */
+@Preview(showBackground = true)
+@Composable
+fun MascotMockupPreview() {
+    Box(
+        modifier = Modifier
+            .size(512.dp)
+            .background(Color(0xFF1A1C1E)),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_mascot_mockup),
+            contentDescription = "Owl Doctor Re-creation Mockup",
+            modifier = Modifier.size(400.dp)
+        )
+    }
+}
